a3274a426f7c38f76bf5d2b9fd4de35ad37a71b6
nmasamba <slimm4rl@yahoo.com>
2018-03-01 17:39:18 +0000

    add URL method

--- 01_get_yahoo.py
+++ 01_get_yahoo.py
@@ -7,3 +7,7 @@ AAPL = Share('AAPL')
 print AAPL.get_price()
 
 pprint(AAPL.get_historical('2014-04-25', '2014-04-29'))
+
+
+# actual URL
+https://finance.yahoo.com/quote/%5EGSPC/history?period1=1519257600&period2=1519689600&interval=1d&filter=history&frequency=1d
