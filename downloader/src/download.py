8e888c7a5086bfebaf81b89a94874d8cec18b5ba
nmasamba <slimm4rl@yahoo.com>
2018-03-14 20:08:22 +0000

    updated to run as script

--- downloader/src/download.py
+++ downloader/src/download.py
@@ -24,14 +24,14 @@ def constructYFURL(ticker, start_date, end_date, freq):
     interval = freq # can be 1d, 1wk or 1mo
     period1 = str(start_date) # format" YYYY-MM-DD
     period2 = str(end_date) # format" YYYY-MM-DD
-    yf_url = "https://finance.yahoo.com/quote/"+ticker+"/history?period1="+start_date+"&period2="+end_date"&interval="+freq"&filter=history&frequency="+freq
+    yf_url = "https://finance.yahoo.com/quote/"+ticker+"/history?period1="+start_date+"&period2="+end_date+"&interval="+freq+"&filter=history&frequency="+freq
     return yf_url
 
 
 
 def download(file_path, url):
     # This function will download data from a URL and save it to a local specified file
-    # Example usage: (1) set up local_path = 'data.csv' (2) pass in a local
+    # Example usage: (1) set up local_path = 'data.txt' (2) pass in a local
     # file path and a download URL to the function e.g. download(local_path, yf_url)
     web_request = urllib2.Request(url)
 
@@ -44,3 +44,12 @@ def download(file_path, url):
 
     except urllib2.HTTPError, e:
         print e.fp.read()
+
+
+if __name__ == "__main__":
+    local_path = "yahoo_data.txt"
+
+    # TO DO: update the URL constructing function so that it can be used here
+    download_url = "https://finance.yahoo.com/quote/NFLX/history?period1=1489521384&period2=1521057384&interval=1d&filter=history&frequency=1d"
+    
+    download(local_path, download_url)
